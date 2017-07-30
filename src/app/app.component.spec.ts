8f07471bd8e0a555b7530d32bc12fd4812f0bb4b
MeghaCasula890 <30486959+MeghaCasula890@users.noreply.github.com>
2017-07-30 12:33:26 -0400

    Update app.component.specs.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,13 @@
 import { TestBed, async } from '@angular/core/testing';
 
-
-import { MaterialModule, MdCardModule, MdGridListModule, MdIconModule, MdToolbarModule, MdButtonModule } from '@angular/material';
-import { RouterModule, Routes } from '@angular/router';
-import { UserInfoComponent } from './user-info/user-info.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { AppRoutingModule } from './app-routing.module';
-import { LogoutComponent } from './account/logout.component';
-import { UserDetailComponent } from './user/detail.component';
-import { APP_BASE_HREF } from '@angular/common';
-
-
-
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      providers: [ {provide: APP_BASE_HREF, useValue: '/'} ],
       declarations: [
-        AppComponent, DashboardComponent, UserInfoComponent, LogoutComponent, UserDetailComponent
+        AppComponent
       ],
-      imports: [ MaterialModule.forRoot(), AppRoutingModule ],
     }).compileComponents();
   }));
 
@@ -31,10 +17,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
+  it(`should have as title 'app works!'`, async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('app works!');
+  }));
+
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('span').textContent).toContain('Vulcan');
+    expect(compiled.querySelector('h1').textContent).toContain('app works!');
   }));
 });
